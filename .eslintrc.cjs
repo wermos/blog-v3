05954f0677daf6afe8de8e0f2d3ed80422e9a732
wermos <63574588+wermos@users.noreply.github.com>
2025-07-03 11:19:09 +0530

    wip commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,21 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'astro'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:astro/recommended',
-    'prettier' // disables conflicting ESLint rules that Prettier handles
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module'
-  },
   overrides: [
     {
       files: ['*.astro'],
       parser: 'astro-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro']
+        extraFileExtensions: ['.astro'],
+        ecmaVersion: 2020,
+        sourceType: 'module'
       },
+      plugins: ['astro'],
+      extends: ['plugin:astro/recommended', 'plugin:@typescript-eslint/recommended', 'eslint:recommended',],
       rules: {
-        // Add any Astro-specific rules here
-        // Example: disable unused vars because Astro includes frontmatter
         'no-unused-vars': 'off'
-      }
-    },
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        // You can put stricter or relaxed TS rules here if needed
+        // You can add Astro-specific rules here
       }
     }
   ],
@@ -38,6 +23,6 @@ module.exports = {
     'node_modules/',
     'dist/',
     '.astro/',
-    'src/components/ui/' // third-party components from shadcn/ui
+    'src/components/ui/'
   ]
 }
