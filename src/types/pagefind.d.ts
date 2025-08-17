4452f738287101e0ae56ecf3368e9d75d9ea7f42
wermos <63574588+wermos@users.noreply.github.com>
2025-08-17 23:42:08 +0530

    Remove a bunch of unused files.

--- src/types/pagefind.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@pagefind/default-ui' {
-  export class PagefindUI {
-    constructor(options: {
-      element: string | HTMLElement;
-      bundlePath: string;
-      [key: string]: any;
-    });
-  }
-}
